2cd5ad9b9da582f508631dbac05cb9df3850f225
anapendot <anaheta.pen@gmail.com>
2022-11-08 21:44:22 -0800

    Update PlayerViews.js

--- morra_final/views/PlayerViews.js
+++ morra_final/views/PlayerViews.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 const exports = {};
 
-// Player views must be extended.
-// It does not have its own Wrapper view.
-
 exports.GetHand = class extends React.Component {
   render() {
     const {parent, playable, hand} = this.props;
@@ -14,18 +11,95 @@ exports.GetHand = class extends React.Component {
         <br />
         {!playable ? 'Please wait...' : ''}
         <br />
+        {'Please pick a hand'}
+        <br />
+        <br />
         <button
           disabled={!playable}
-          onClick={() => parent.playHand('ROCK')}
-        >Rock</button>
+          onClick={() => parent.playHand(0)}
+        >Zero</button>
         <button
           disabled={!playable}
-          onClick={() => parent.playHand('PAPER')}
-        >Paper</button>
+          onClick={() => parent.playHand(1)}
+        >One</button>
         <button
           disabled={!playable}
-          onClick={() => parent.playHand('SCISSORS')}
-        >Scissors</button>
+          onClick={() => parent.playHand(2)}
+        >Two</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(3)}
+        >Three</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(4)}
+        >Four</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(5)}
+        >Five</button>
+      </div>
+    );
+  }
+}
+
+exports.GetGuess = class extends React.Component{
+  render(){
+    //constants ?
+    const {parent, playable, guess} = this.props;
+    return(
+      <div>
+        {guess ? 'It was a draw, guess again!' : ''}
+        <br />
+        {!playable ? 'Please Wait...' : ''}
+        <br />
+        {'Please guess the total'}
+        <br />
+        <br />
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuess(0)}
+        >Zero</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(1)}
+        >One</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(2)}
+        >Two</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(3)}
+        >Three</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(4)}
+        >Four</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(5)}
+        >Five</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(6)}
+        >Six</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(7)}
+        >Seven</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(8)}
+        >Eight</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(9)}
+        >Nine</button>
+        <button
+          disable={!playable}
+          onClick={() => parent.playGuess(10)}
+        >Ten</button>
       </div>
     );
   }
